779c886087f401bd04052b2de16f271e01110848
lucky <takuto.yoshikai@gmail.com>
2021-01-07 17:33:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# silver
+An install script of Takuto Yoshikai's hack tools.
+
+### install
+
+```bash
+git clone https://github.com/TakutoYoshikai/silver.git
+cd silver
+./install.sh
+```
+
+### License
+MIT License
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd `dirname $0`
+function gitclone () {
+  git clone https://github.com/TakutoYoshikai/$1.git
+}
+
+function pipinstall() {
+  pip3 install git+https://github.com/TakutoYoshikai/$1.git
+}
+
+pipinstall angie-sirius
+pipinstall phalanx
+pipinstall elemental-sight
+pipinstall mist-dispersion
+pipinstall miki
+
+gitclone gram-demolition
+gitclone nats
+gitclone shiba
+cd shiba
+./install.sh
+cd ..
